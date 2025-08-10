73a2b65902f67551681c02427b064240ed828412
Eng_3bdo_7amed <3bdo.7amed.com@gmail.com>
2025-08-10 03:31:27 +0300

    Connecting to the database

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mongoose = require("mongoose");
+const db_password = "ABDOalaa177213";
+const uri = `mongodb+srv://Eng_3bdo_7amed:${db_password}@cluster0.hqrcc64.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.get("/", (req, res) => {
   //   res.send("<h1>Hello World!</h1>");
   res.sendFile(__dirname + "/views/index.html");
